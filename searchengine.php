90d8077086a2a0ef8893ab1f0e4576fb7f79ebdd
mikolajmarcinczak <mikolajmarcinczak@users.noreply.github.com>
2019-05-09 10:57:09 +0200

    hgf

---
+++ searchengine.php
@@ -0,0 +1,6 @@
+﻿<?php 
+    $result = $_POST['search'];
+    $result = strtolower($result);
+
+    if($result == "index" || $result == "glowna" || $result == "strona glowna" || $result == "strona główna" || )
+?>
