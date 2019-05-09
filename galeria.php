ea706d6c82035127f47e31cb02f423ad1a60786e
mikolajmarcinczak <mikolajmarcinczak@users.noreply.github.com>
2019-05-09 10:32:34 +0200

    duzo zmian php i w ogole

--- galeria.php
+++ galeria.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="pl">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aba Glass - Galeria</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">   
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" type="text/css" href="socialmedia.css">
   </head>
@@ -73,10 +73,62 @@
         </a>
       </div>
       <!--tu bedzie jakis skrypcik na fotogalerie-->
-      <div class="container">
-        <nav></nav>
-        <section></section>
-        <footer></footer>
+      <div class='container'>
+        <div class="gallery">
+        
+         <?php 
+         $image_extensions = array("png","jpg","jpeg","gif","PNG","JPG","JPEG","GIF");
+       
+         $dir = 'images/phocagallery/';
+
+        $dir_alt_l = "images/phocagallery/thumbs/phoca_thumb_l_";
+        $dir_alt_m = "images/phocagallery/thumbs/phoca_thumb_m_";
+
+         if (is_dir($dir)){
+        
+          if ($dh = opendir($dir)){
+           $count = 1;
+       
+           while (($file = readdir($dh)) !== false){
+       
+            if($file != '' && $file != '.' && $file != '..'){
+        
+             if (!file_exists($dir_alt_l.$file)) {
+                $thumbnail_path = $dir_alt_m.$file;
+             }
+             else $thumbnail_path = $dir_alt_l.$file;
+       
+             $image_path = $dir.$file;
+        
+             $thumbnail_ext = pathinfo($thumbnail_path, PATHINFO_EXTENSION);
+             $image_ext = pathinfo($image_path, PATHINFO_EXTENSION);
+       
+             if(!is_dir($image_path) && 
+                in_array($thumbnail_ext,$image_extensions) && 
+                in_array($image_ext,$image_extensions)){
+          ?>
+       
+              <a href="<?php echo $image_path; ?>">
+               <img src="<?php echo $thumbnail_path; ?>" alt="" title=""/>
+              </a>
+              <?php
+       
+              if( $count%4 == 0){
+              ?>
+                <div class="clear"></div>
+              <?php 
+              }
+              $count++;
+             }
+            }
+        
+           }
+           closedir($dh);
+          }
+         }
+        ?>
+        </div>
+       </div>
       </div>
       <aside id="sticky-social">
         <ul>
@@ -90,8 +142,16 @@
       <a class="footer" href="http://opentechnologies.pl"><img src="img/logoopen.png"></a>
       <span class="footertext">&copy; Aba Glass 2018 Łukasz Wolniak, Łukasz Nolka, Michał Roszyk & Leszek Matuszewski</span>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    
+    <link href='lightbox/dist/simplelightbox.min.css' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="lightbox/dist/simple-lightbox.js"></script> 
+    <script type='text/javascript'>
+      $(document).ready(function() {
+        var gallery = $('.gallery a').simpleLightbox();
+      });
+    </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
